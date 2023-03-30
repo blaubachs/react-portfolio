f8ca70390b30c35eefe0445dc282da9dba59f903
Ben L <veloswim@gmail.com>
2023-03-30 11:33:31 -0700

    moved icons to header section

--- src/App.js
+++ src/App.js
@@ -4,25 +4,26 @@ import me from "./assets/img/me.jpg";
 function App() {
   return (
     <div className="App">
-      <header>
+      <header className="hero">
         <h1>developing.this</h1>
         <Avatar alt="Ben Laubach" src={me} sx={{ width: 300, height: 300 }} />
-        <p>Bingus</p>
+        <p>
+          Full-stack web developer, musician, and technologist with a passion
+          for creative problem solving and furthering my knowledge.
+        </p>
+        <div className="icons">
+          <img
+            alt="github logo"
+            height="40"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+          />
+          <img
+            alt="linkedIn"
+            height="40"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg"
+          />
+        </div>
       </header>
-
-      <footer>
-        <img
-          alt="github logo"
-          height="40"
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-        />
-
-        <img
-          alt="linkedIn"
-          height="40"
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg"
-        />
-      </footer>
     </div>
   );
 }
