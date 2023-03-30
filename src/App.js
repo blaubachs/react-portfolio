576ac9cf6bf152a221ed51f0d86ec9a53acea085
Ben L <veloswim@gmail.com>
2023-03-30 11:24:52 -0700

    added mui avatar and devicons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
-
+import Avatar from "@mui/material/Avatar";
+import me from "./assets/img/me.jpg";
 function App() {
   return (
     <div className="App">
       <header>
-        <h1>bingus</h1>
+        <h1>developing.this</h1>
+        <Avatar alt="Ben Laubach" src={me} sx={{ width: 300, height: 300 }} />
+        <p>Bingus</p>
       </header>
+
+      <footer>
+        <img
+          alt="github logo"
+          height="40"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+        />
+
+        <img
+          alt="linkedIn"
+          height="40"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg"
+        />
+      </footer>
     </div>
   );
 }
