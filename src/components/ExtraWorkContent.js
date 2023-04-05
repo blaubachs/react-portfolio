72058eae7b994771f2602c31fa8d6adea236a803
Ben L <veloswim@gmail.com>
2023-04-04 18:28:29 -0700

    added row class to extra work card content, goto and small github icons added

--- src/components/ExtraWorkContent.js
+++ src/components/ExtraWorkContent.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "../assets/css/ExtraWorkContent.css";
 
+// these needs to accept props for each component
 export default function ExtraWorkContent() {
   return (
     <div id="extra-card-content">
       <div className="extra-content-row">
         <h2 className="no-margin-top">Role</h2>
         <p>Project Manager</p>
+      </div>
+      <div className="extra-content-row">
         <h2 className="no-margin-top">Timeframe</h2>
         <p>2 Weeks</p>
       </div>
