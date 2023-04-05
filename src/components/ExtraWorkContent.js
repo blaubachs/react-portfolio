46e99c0ff7f89383ca0d5296c8a0fec7a227f8d7
Ben L <veloswim@gmail.com>
2023-04-04 19:33:47 -0700

    adjusted problem and process row

--- src/components/ExtraWorkContent.js
+++ src/components/ExtraWorkContent.js
@@ -1,17 +1,28 @@
 import React from "react";
+import FadeIn from "./FadeIn";
 import "../assets/css/ExtraWorkContent.css";
 
 // these needs to accept props for each component
 export default function ExtraWorkContent() {
   return (
     <div className="extra-card-content">
-      <div className="extra-content-row">
+      <div className="top-row">
         <h2 className="no-margin-top">Role</h2>
         <p>Project Manager</p>
       </div>
-      <div className="extra-content-row">
+      <div className="top-row">
         <h2 className="no-margin-top">Timeframe</h2>
         <p>2 Weeks</p>
+      </div>
+      <div className="extra-content-row">
+        <div className="extra-content-text">
+          <h2>Problem</h2>
+          <p>Problem statement from presentation</p>
+        </div>
+        <div className="extra-content-text">
+          <h2>Process</h2>
+          <p>Development process</p>
+        </div>
       </div>
     </div>
   );
